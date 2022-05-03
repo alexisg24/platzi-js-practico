ba3a1f011233f1f4805f2e7ca4f52c2edfcefc5e
Alexis Garcia <azoreno2@gmail.com>
2022-05-02 22:54:05 -0400

    Maquetado de pagina web dinamica para caluclar area, perimetro y altura de figuras geometricas

--- figuras.js
+++ figuras.js
@@ -1,30 +1,79 @@
 //Cuadrado
-console.group("Cuadrados");
 const perimetroCuadrado = (lado) => lado * 4;
 const areaCuadrado = (lado) => lado * lado;
 
-console.log(`El perimetro del cuadrado es ${perimetroCuadrado(4)}cm^2`);
-console.log(`El area del cuadrado es ${areaCuadrado(5)}cm^2`);
-console.groupEnd();
 
 //Triangulo
-console.group("Triangulos");
-const perimetroTriangulo = (lado1, lado2, base) => lado1+lado2+base;
+const perimetroTriangulo = (lado1, lado2, base) => parseInt(lado1)+parseInt(lado2)+parseInt(base);
 const areaTriangulo = (base, altura) => (base*altura)/2;
 
-console.log(`El perimetro del triangulo es de ${perimetroTriangulo(7,5,6)}cm`);
-console.log(`El area del triangulo es de ${areaTriangulo(6,8)}cm^2`);
-console.groupEnd();
 
 //Circulo
-console.group("Circulos");
 const diametro = (radio) => radio*2;
 const pi = Math.PI;
 const perimetroCirculo = (radio) => diametro(radio)*pi;
 const areaCirculo = (radio) => (radio*radio)*pi;
 
-console.log(`El diametro del circulo es de ${diametro(4)}cm`);
-console.log(`Pi es: ${pi}`);
-console.log(`El perimetro del circulo es de ${perimetroCirculo(8)}cm`);
-console.log(`El area del circulo es de ${areaCirculo(4)}cm^2`);
-console.groupEnd();
+//Dinamismo
+//Cuadrado
+const calculoPerimetroC = () =>{
+    const lado = document.getElementById("cuadrado").value;
+    const res = perimetroCuadrado(lado);
+    document.getElementById("cuadradoResult").value = res;
+};
+
+const calculoAreaC = () =>{
+    const lado = document.getElementById("cuadrado").value;
+    const res = areaCuadrado(lado);
+    document.getElementById("cuadradoResult").value = res;
+};
+
+//Triangulo
+const calculoPerimetroT = () =>{
+    const base = document.getElementById("base").value;
+    const lado1 = document.getElementById("lado1").value;
+    const lado2 = document.getElementById("lado2").value;
+    const res = perimetroTriangulo(lado1, lado2, base);
+    document.getElementById("trianguloResult").value = res;
+};
+
+const calculoAreaT = () =>{
+    const base = document.getElementById("base").value;
+    const altura = document.getElementById("altura").value;
+    const res = areaTriangulo(Number(base), altura);
+    document.getElementById("trianguloResult").value = res;
+};
+
+const calculoAlturaTI = () =>{
+    const base = document.getElementById("base").value;
+    const lado1 = document.getElementById("lado1").value;
+    const lado2 = document.getElementById("lado2").value;
+    parseInt(lado1,lado2,base);
+    if(lado1 == lado2 && lado1 != base){
+        const altura = Math.sqrt((lado1**2) - ((lado2**2)/4))
+        document.getElementById("altura").value = altura;
+
+    }else{
+        alert("No es isosceles")
+    }
+};
+
+
+//Circulo
+const calculoDiametroC = () =>{
+    const radio = document.getElementById("radio").value;
+    const res = diametro(radio);
+    document.getElementById("circuloResult").value = res;
+};
+
+const calculoPerimetroCi = () =>{
+    const radio = document.getElementById("radio").value;
+    const res = perimetroCirculo(radio);
+    document.getElementById("circuloResult").value = res;
+};
+
+const calculoAreaCi = () =>{
+    const radio = document.getElementById("radio").value;
+    const res = areaCirculo(radio);
+    document.getElementById("circuloResult").value = res;
+};
