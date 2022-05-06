56ad5b070286cb88dd9d40835b830f11896947b7
Alexis Garcia <azoreno2@gmail.com>
2022-05-06 17:25:55 -0400

    Realizacion de algoritmos y aplicacion dentro del html

--- descuentos.js
+++ descuentos.js
@@ -5,3 +5,39 @@ const calculatePriceWitDiscount = (price, disc) => {
 }
 
 calculatePriceWitDiscount(180,62);
+
+const printDiscount = () =>{
+    const price = document.getElementById("price").value;
+    const disc = document.getElementById("discount").value;
+    const couponUser = document.getElementById("coupon").value;
+    let discFinal = parseInt(disc);
+    const coupons = [
+        {
+            name:"platzi",
+            discount:10,
+        },
+        {
+            name:"js",
+            discount:15,
+        },
+        {
+            name:"html",
+            discount:5,
+        },
+    ];
+
+    const validCoupon = coupons.find(coupon => coupon.name == couponUser);
+    
+    if (!validCoupon) {
+        document.getElementById("cdisc").value = "Cupon Invalido"
+    }else{
+        document.getElementById("cdisc").value = `Cupon aplicado ${validCoupon.discount}% de dcto adicional.`;
+        discFinal += parseInt(validCoupon.discount);
+    }
+
+    console.log(discFinal);
+
+    const finalPrice = calculatePriceWitDiscount(price, discFinal);
+    document.getElementById("resultP").innerText = `El precio con descuento es de $${finalPrice}`;
+}
+
